6fe2963ed7aebd141cb3514417a229f756701e63
Kurt Jing <wzs129@hotmail.com>
2019-12-20 12:21:46 +1100

    init

---
+++ makeHostAndCert.js
@@ -0,0 +1,17 @@
+const devcert = require('devcert');
+const domain = process.argv[2];
+const fs = require('fs');
+
+const makeFile = (name, content) => {
+  fs.writeFile(name, content, err => {
+      if (err) throw err;
+      console.log(`${name} CREATED SUCCESSFULLY`);
+  });
+};
+const ssl = async function (domain) {
+	console.log(domain)
+  const s = await devcert.certificateFor(domain);
+  makeFile(`./certs/${domain}.crt`, s.cert);
+  makeFile(`./certs/${domain}.key`, s.key);
+};
+ssl(domain);
